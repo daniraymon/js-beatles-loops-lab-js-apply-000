ef76047a3cde93dfbd55bd4c1c5f2eacb2a9e522
Daniel Raymon <daniraymon@gmail.com>
2017-09-09 00:23:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musiciansArray, instrumentsArray) {
   var newArray = [];
   for (var i = 0; i < musiciansArray.length; i++) {
     var musician = musiciansArray[i];
-    var sent = `$(musician) plays $(instrumentsArray[0])`;
+    var sent = musician + " plays "instrumentsArray[0];
     newArray.push(sent);
   }
   return newArray;
